2d6685c42ace424ab869570fda84237e8be25103
Cristiano Pontes <pontescr@gmail.com>
2022-05-02 21:10:41 -0300

    Change spring 2.3.x and hr-worker implementation

---
+++ hr-worker/src/main/java/com/pontescr/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.pontescr.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pontescr.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}

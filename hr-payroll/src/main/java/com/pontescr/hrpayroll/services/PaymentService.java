9dd0908b3f51c15b59199ae3fea9e9b623dc34e1
Cristiano Pontes <pontescr@gmail.com>
2022-05-04 21:39:16 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/pontescr/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/pontescr/hrpayroll/services/PaymentService.java
@@ -1,13 +1,34 @@
 package com.pontescr.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.pontescr.hrpayroll.entities.Payment;
+import com.pontescr.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Bob", 200.00, days);
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class,uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }

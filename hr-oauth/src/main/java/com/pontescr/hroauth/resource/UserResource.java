432c02baee6835724b70a8d3c9cf4f73a7f92de6
Cristiano Pontes <pontescr@gmail.com>
2022-05-16 22:58:59 -0300

    UserFeignClients

--- hr-oauth/src/main/java/com/pontescr/hroauth/resource/UserResource.java
+++ hr-oauth/src/main/java/com/pontescr/hroauth/resource/UserResource.java
@@ -1,32 +1,31 @@
-package com.pontescr.hruser.resources;
+package com.pontescr.hroauth.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pontescr.hruser.entitieis.User;
-import com.pontescr.hruser.repositories.UserRepository;
+import com.pontescr.hroauth.entities.User;
+import com.pontescr.hroauth.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
 	@Autowired
-	private UserRepository repository;
-	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User obj = repository.findById(id).get();
-		return ResponseEntity.ok().body(obj);
-	}
+	private UserService service;
 	
 	@GetMapping(value = "/search")
 	public ResponseEntity<User> findByEmail(@RequestParam String email) {
-		User obj = repository.findByEmail(email);
+		try {
+		User obj = service.findByEmail(email);
 		return ResponseEntity.ok().body(obj);
+		}
+		catch(IllegalArgumentException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
 	}
 }

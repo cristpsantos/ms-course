631f0e9eeeaaad16cbf36270837f6fefc11e63d5
Cristiano Pontes <pontescr@gmail.com>
2022-05-16 22:08:38 -0300

    Configuring hr-oauth project

--- hr-oauth/src/main/java/com/pontescr/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/pontescr/hroauth/HrOauthApplication.java
@@ -1,8 +1,10 @@
-package com.pontescr.hrconfigserver;
+package com.pontescr.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrOauthApplication {
 

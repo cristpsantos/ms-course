f4f8a302184dd4e7659ceee842b5be2592ba457e
Cristiano Pontes <pontescr@gmail.com>
2022-05-17 21:57:40 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/pontescr/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/pontescr/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.pontescr.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.pontescr.hroauth.entities.User;
 import com.pontescr.hroauth.feignclients.UserFeignClients;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -25,4 +28,17 @@ public class UserService {
 		logger.info("Email found" + email);
 		return user;
 	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClients.findByEmail(username).getBody();
+		if(user == null ) {
+			logger.error("Email not found" + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found" + username);
+		return user;
+	}
+	
+	
 }

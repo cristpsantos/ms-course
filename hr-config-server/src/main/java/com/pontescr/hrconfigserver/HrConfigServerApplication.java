741b9bd5e5b8e7be8da690eb6c739fc64384e786
Cristiano Pontes <pontescr@gmail.com>
2022-05-19 21:39:29 -0300

    db config server

--- hr-config-server/src/main/java/com/pontescr/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/pontescr/hrconfigserver/HrConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.pontescr.hrconfigserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,20 +9,14 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class HrConfigServerApplication implements CommandLineRunner{
 
-	@Value("${spring.cloud.config.server.git.username}")
-	private String username;
-	
-	@Value("${spring.cloud.config.server.git.password}")
-	private String password;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(username);
-		System.out.println(password);
+		//System.out.println(username);
+		//System.out.println(password);
 		
 	}
 
--- hr-worker/src/main/java/com/pontescr/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/pontescr/hrworker/resources/WorkerResource.java
@@ -22,9 +22,6 @@ import com.pontescr.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
-	@Value("${test.config}")
-	private String testConfig;
-	
 	@Autowired
 	private Environment env;
 	
@@ -35,7 +32,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
